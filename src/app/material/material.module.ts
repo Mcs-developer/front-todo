148ffa239dbece204e01a3a0dfb2974e5bbee1d9
Marlon C.S <marlsas0306@gmail.com>
2018-09-30 21:09:30 -0500

    new material module

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material';
+
+@NgModule({
+  imports: [MatInputModule],
+  exports: [MatInputModule],
+  declarations: []
+})
+export class MaterialModule { }

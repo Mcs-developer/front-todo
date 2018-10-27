2da70e2dfc7804042e2d139f7065ac1fd89bff9b
Marlon C.S <marlsas0306@gmail.com>
2018-10-27 18:56:47 -0500

    add some modules to matmodule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
-import { MatInputModule } from '@angular/material';
+import { MatInputModule, MatFormFieldModule, MatButtonModule, MatSelectModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [MatInputModule],
-  exports: [MatInputModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule
+  ],
+  exports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule
+  ],
   declarations: []
 })
 export class MaterialModule { }

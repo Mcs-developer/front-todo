d7c7df306762ee10e4e990f5bd8f755ae98346cf
Marlon C.S <marlsas0306@gmail.com>
2018-10-27 18:58:33 -0500

    apply guard-import

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { throwIfAlreadyLoaded } from './module-import-guard';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [],
+  exports: [
+  ],
   declarations: []
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}

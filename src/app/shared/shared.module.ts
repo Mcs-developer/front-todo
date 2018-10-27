76da55c634b0351b3e1f67675aa5e0732473b76c
Marlon C.S <marlsas0306@gmail.com>
2018-10-27 18:59:51 -0500

    clean shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [
-    MatInputModule
-  ],
-  exports: [
-    MatInputModule
-  ],
+  imports: [],
   declarations: []
 })
 export class SharedModule { }

e10bf8e1939808094f6835f705a3351d6390e2d8
Marlon C.S <marlsas0306@gmail.com>
2018-10-27 21:40:52 -0500

    progress login style

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }

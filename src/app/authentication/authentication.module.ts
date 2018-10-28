31f0e58dca32b2d5f0fc0e981c664c623e67992a
Marlon C.S <marlsas0306@gmail.com>
2018-10-27 21:38:36 -0500

    add reactiveforms to authentication module

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
+    ReactiveFormsModule,
     AuthenticationRoutingModule
   ],
   declarations: [LoginComponent]

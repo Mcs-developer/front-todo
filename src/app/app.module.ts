cb4e2df5814f94a529a8b498f7d1d9ec6941f06a
Marlon C.S <marlsas0306@gmail.com>
2018-11-18 20:34:33 -0500

    delete share module from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 
@@ -16,7 +15,6 @@ import { AuthenticationModule } from './authentication/authentication.module';
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
-    SharedModule,
     AuthenticationModule,
     AppRoutingModule
   ],

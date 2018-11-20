63082f193613ff46c4a04548980ddfc1f25b1b52
Marlon C.S <marlsas0306@gmail.com>
2018-11-20 14:28:00 -0500

    add navbar to module

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { TasksRoutingModule } from './tasks-routing.module';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { MaterialModule } from '../material/material.module';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    TasksRoutingModule
+    TasksRoutingModule,
+    MaterialModule
   ],
-  declarations: [TasksListComponent, TasksComponent]
+  declarations: [
+    TasksListComponent,
+    TasksComponent,
+    NavbarComponent
+  ]
 })
 export class TasksModule { }

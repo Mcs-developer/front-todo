c53edc96e4557fff5e3fd4943da4659f1748379d
Marlon C.S <marlsas0306@gmail.com>
2018-11-23 13:35:22 -0500

    progress design component tasks list

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-list.component.scss']
 })
 export class TasksListComponent implements OnInit {
+  tasks: any;
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout( () => this.loadTask(), 1000);
   }
 
+  loadTask() {
+    this.tasks = [
+      1, 2, 3, 5, 6
+    ];
+  }
 }
